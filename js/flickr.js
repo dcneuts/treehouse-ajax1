50c519ca9179e4f5c657433580abeb8194ac124e
dcneuts <dcneuts@gmail.com>
2017-04-30 18:49:15 -0700

    Fixed a few minor errors in JS, then added functionality for status button when searching by user-defined tag

--- js/flickr.js
+++ js/flickr.js
@@ -27,6 +27,10 @@ $(document).ready(function () {
 	$('form').submit(function(evt) {
 		evt.preventDefault();//stop normal behavior of browser
 		$searchField = $('#search');//text field for search window
+		$submitButton = $('#submit');
+		//disable search field
+		$searchField.prop("disabled", true);
+		$submitButton.attr("disabled", true).val("pulling...");
 		var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 		var animal = $searchField.val();
 		var flickrOptions = {
@@ -42,6 +46,10 @@ $(document).ready(function () {
 			});
 			photoHTML += '</ul>';
 			$('#photos').html(photoHTML);
+			//had to re-activate the search button and name after search was over, must place it here due to
+			// callback position
+			$searchField.prop("disabled", false);
+			$submitButton.attr("disabled", false).val('Search')
 		}
 		$.getJSON(flickerAPI, flickrOptions, displayPhotos);
 	});
